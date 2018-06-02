cd44c6290b3a7be8972bd50d979cef2264877625
Avinash54Bharti <avinash.bharti.met17@itbhu.ac.in>
2018-06-02 19:30:37 +0530

    sdfghbnf

---
+++ file/script.js
@@ -0,0 +1,8 @@
+function dropclick() {
+    document.getElementById('drop').className +=" show";
+    console.log("tdfhj");
+}
+function dropback() {
+    document.getElementById('drop').className ="row";
+
+}
